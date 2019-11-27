81cae302f3ccd081da56a4f6ebe39101b0bbcdcf
steve <stevenzearo@gmail.com>
2019-11-27 16:56:36 +0800

    init

---
+++ src/test/scala/steve/leetcode/Solution.scala
@@ -0,0 +1,40 @@
+package steve.leetcode
+
+/**
+ * @author steve
+ *
+ *         你的初始能量为 P，初始分数为 0，只有一包令牌。
+ *
+ *         令牌的值为 token[i]，每个令牌最多只能使用一次，可能的两种使用方法如下：
+ *
+ *         如果你至少有 token[i] 点能量，可以将令牌置为正面朝上，失去 token[i] 点能量，并得到 1 分。
+ *         如果我们至少有 1 分，可以将令牌置为反面朝上，获得 token[i] 点能量，并失去 1 分。
+ *
+ *         在使用任意数量的令牌后，返回我们可以得到的最大分数。
+ *
+ *         来源：力扣（LeetCode）
+ *         链接：https://leetcode-cn.com/problems/bag-of-tokens
+ *         著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+object Solution {
+    def bagOfTokensScore(tokens: Array[Int], P: Int): Int = {
+      var score: Int = 0
+      val i: Int = 0
+      var energy: Int = tokens(i)
+      val turnUp: Boolean = false
+      if (P > tokens(i)) {
+        energy -= tokens(i)
+        score += 1
+      }
+      // todo get max score
+      return score
+      0
+    }
+
+  def bagOfTokensScore(tokens: Array[Int], P: Int, score: Int): (Array[Int], Int, Int) = {
+
+    while (P > tokens.min)
+
+    (Array(0), 0, 0)
+  }
+}
