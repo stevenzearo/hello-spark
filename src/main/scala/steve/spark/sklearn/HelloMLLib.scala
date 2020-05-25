5e4a4514d8beb1afeb906fd8b6958f871c432d2e
steve <stevezou@chancetop.com>
2020-05-25 02:20:50 -0400

    init

--- src/main/scala/steve/spark/sklearn/HelloMLLib.scala
+++ src/main/scala/steve/spark/sklearn/HelloMLLib.scala
@@ -1,13 +1,21 @@
 package steve.spark.sklearn
 
+import org.apache.spark.ml.attribute.Attribute
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.LinearDataGenerator
+
+import scala.collection.{immutable, mutable}
 
 
 /**
  * @author steve
  */
-class HelloMLLib {
+object HelloMLLib {
   def main(args: Array[String]): Unit = {
+    val list: mutable.Seq[LabeledPoint] = LinearDataGenerator.generateLinearInput(1, Array(1), Array(1), Array(1), 100, 100, 0, 1).toBuffer
+    val buffer: mutable.Seq[(Double, Double)] = list.map(value => (value.features.apply(0), value.label)).toBuffer
+    buffer.foreach(x => println(x._1 + "*1 + 1 = " + (x._1*1 + 1) + "\t\t\t" + x._2))
   }
 }
